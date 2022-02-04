dc200923756da4a4b03e9615dba80778a3c60a52
Marcin Bednarz <94953520+Yerbaneyro@users.noreply.github.com>
2022-02-04 01:08:30 +0100

    Initial
    Initial Repository...main Home Screen styling implemented. Toggle button for Dark Mode styled and working. Animation imported.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BiHomeAlt, BiCube, BiUserCircle,  } from 'react-icons/bi';
+import { useSpring, animated } from 'react-spring';
+import { FaHtml5, FaCss3, FaReact, FaSass, FaLinux, FaNodeJs, FaPython, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { SiJavascript, SiTypescript, SiGit } from 'react-icons/si'
+import { FiMail } from 'react-icons/fi'
+import ToggleButton from './components/toggle-button'
+
+
+function IconsSlider() {
+  return (
+    <div className='icon-container'>
+      <FaHtml5 />
+      <FaCss3 />
+      <SiJavascript /> 
+      <SiTypescript /> 
+      <FaSass />
+      <FaReact />
+      <FaNodeJs />
+      <FaPython />
+      <SiGit />
+      <FaLinux />
+    </div>
+  )
+}
+
+function ContactButtons() {
+  return(
+    <div className='social-buttons'>
+      <div>
+        <FaGithub />
+        <FaLinkedin />
+      </div>
+      <div>
+        <FiMail />
+        <p className='cv'>CV</p>
+      </div>  
+    </div>
+  )
+}
+
+function Navigation() {
+  
+  return (
+    <div className='nav-bar'>
+      <div>
+        <p className='home'><BiHomeAlt className='icon'/>Home</p>
+      </div>
+      <div>
+      <p className='portfolio'><BiCube className='icon'/>Projects</p>      
+      </div>
+      <div>
+        <p className='about-me'><BiUserCircle className='icon'/>About Me.</p>
+      </div>
+    </div>
+  )
+}
+
 
 function App() {
+
+  const props = useSpring({
+    to: {opacity: 1, scale: 1.3},
+    from: { opacity: 0, scale: 1},
+    config: { friction: 70 },
+    }
+  );
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ContactButtons />
+      <ToggleButton />
+      <Navigation /> 
+      <div className='center'>
+            <animated.h1 style={props} className={'myName'}>Marcin Bednarz</animated.h1>
+            <h2>Web Developer</h2>
+      </div> 
+      <IconsSlider />
+      
     </div>
   );
 }
---
+++ src/components/toggle-button.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './toggle-button.css'
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+export default function ToggleButton(props) {
+
+    const { disabled, className } = props;
+
+    const [toggle, setToggle] = useState(false);
+
+    const triggerToggle = () => {
+
+        if ( disabled ) { return; }
+        setToggle( !toggle )
+    }
+
+    const toggleClasses = classNames('wrg-toggle', {
+        'wrg-toggle--checked': toggle,
+        'wrg-toggle--disabled': disabled
+    }, className);
+
+    return (
+        <div onClick={triggerToggle} className={toggleClasses}>
+            <div className="wrg-toggle-container">
+                <div className="wrg-toggle-check">
+                    <span>🌜</span>
+                </div>
+                <div className="wrg-toggle-uncheck">
+                    <span>🌞</span>
+                </div>
+            </div>
+            <div className="wrg-toggle-circle"></div>
+            <input className="wrg-toggle-input" type="checkbox" aria-label="Toggle Button" />
+        </div>
+    )
+}
+
+ToggleButton.propTypes = {
+    disabled: PropTypes.bool,
+    defaultChecked: PropTypes.bool,
+    className: PropTypes.string,
+    onChange: PropTypes.func,
+    icons: PropTypes.oneOfType([
+        PropTypes.bool,
+        PropTypes.shape({
+            checked: PropTypes.node,
+            unchecked: PropTypes.node
+        })
+    ])
+};
